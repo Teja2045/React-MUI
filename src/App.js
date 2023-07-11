58f0c34c457faf7564ea9f5ce0f4d0cf03973962
saiteja <tejargukt123@gmail.com>
2023-07-11 11:16:41 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import MuiTypography from './MuiTypography';
+import MuiButton from './MuiButton';
 function App() {
   return (
     <div className="App">
       <MuiTypography/>
+      <MuiButton/>
     </div>
     
   );
---
+++ src/MuiButton.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Stack, Button } from "@mui/material";
+const MuiButton = () => {
+  return (
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+      <Stack direction="row" spacing={2}>
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+      </Stack>
+      <Stack direction="row" spacing={2}>
+        <Button variant="text" color="primary">
+          Primary
+        </Button>
+        <Button variant="text" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="text" color="error">
+          Error
+        </Button>
+        <Button variant="outlined" color="warning">
+          Warning
+        </Button>
+        <Button variant="outlined" color="info">
+          Info
+        </Button>
+        <Button variant="outlined" color="success">
+          Success
+        </Button>
+      </Stack>
+      <Stack display="block" spacing={2} direction="row">
+        <Button variant="contained" size="small">
+            Small
+        </Button>
+        <Button variant="contained" size="large">
+            Large
+        </Button>
+        <Button variant="contained" size="medium">
+            Medium
+        </Button>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default MuiButton;
